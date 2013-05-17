ea5c89072da6396461e356d5b44828220aea6b2d
Konstantin Kovshenin <kovshenin@gmail.com>
2013-05-17 08:04:23 +0400

    Bump version numbers.

--- functions.php
+++ functions.php
@@ -103,8 +103,8 @@ add_action( 'widgets_init', 'mag_widgets_init' );
  * Enqueue scripts and styles
  */
 function mag_scripts() {
-	wp_enqueue_style( 'mag-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'style-less', get_stylesheet_directory_uri() . '/mag.css', array( 'mag-style' ), 1 );
+	wp_enqueue_style( 'mag-style', get_stylesheet_uri(), array(), 2 );
+	wp_enqueue_style( 'style-less', get_stylesheet_directory_uri() . '/mag.css', array( 'mag-style' ), 2 );
 
 	wp_enqueue_script( 'mag-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
