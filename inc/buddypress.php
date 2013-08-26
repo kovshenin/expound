5a5451ce41eacd0cbdce0949ae63368294255383
Konstantin Kovshenin <kovshenin@gmail.com>
2013-08-26 15:23:51 +0400

    Minor code cleanup.

--- inc/buddypress.php
+++ inc/buddypress.php
@@ -1,6 +1,9 @@
 <?php
+/**
+ * BuddyPress Compatibility
+ */
 
-add_action( 'wp_enqueue_scripts', 'expound_bp_css' );
 function expound_bp_css(){
-    wp_enqueue_style( 'expound-buddypress', get_stylesheet_directory_uri() . '/css/bp.css' );
+    wp_enqueue_style( 'expound-buddypress', get_template_directory_uri() . '/css/buddypress.css' );
 }
+add_action( 'wp_enqueue_scripts', 'expound_bp_css' );
